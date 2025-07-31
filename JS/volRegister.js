f1824d9d59333bc8ec506aab8733a9ed85d63ca0
Martine Caasi <Martinecaasi@gmail.com>
2025-07-31 16:23:54 +0300

    Update volRegister.js

--- JS/volRegister.js
+++ JS/volRegister.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     try {
-      const response = await fetch('https://handsonserver.onrender.com/api/volunteers', {
+      const response = await fetch('https://handsonserver-new.onrender.com/api/volunteers', {
         method: 'POST',
         body: formData 
       });
